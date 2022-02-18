ab2d26d92387ecce657a0397623c536b53c81d06
Leo Generali <leo.generali@vimeo.com>
2022-02-18 11:18:52 -0500

    Add caching

--- src/_data/notion.js
+++ src/_data/notion.js
@@ -3,5 +3,6 @@ const Notion = require("../_util/notion");
 module.exports = async function () {
   const tils = await Notion.getTILs();
   const home = await Notion.getHomePage();
+
   return { tils, home };
 };
--- src/_util/notion.js
+++ src/_util/notion.js
@@ -1,6 +1,9 @@
+const { AssetCache } = require("@11ty/eleventy-cache-assets");
 const { Client } = require("@notionhq/client");
 const { NotionToMarkdown } = require("notion-to-md");
 const slugify = require("slugify");
+
+const CACHE_TIME = "30m";
 
 const notion = new Client({
   auth: process.env.NOTION_KEY,
@@ -11,17 +14,47 @@ const n2m = new NotionToMarkdown({
 });
 
 async function getTILData() {
-  return await notion.databases.query({
+  const tilDataCache = new AssetCache("TIL_DATA");
+
+  if (tilDataCache.isCacheValid(CACHE_TIME)) {
+    return tilDataCache.getCachedValue();
+  }
+
+  const tils = await notion.databases.query({
     database_id: process.env.NOTION_TILS,
   });
+
+  await tilDataCache.save(tils, "json");
+
+  return tils;
+}
+
+async function pageToMarkdown(id) {
+  const pageDataCache = new AssetCache(id);
+
+  if (pageDataCache.isCacheValid(CACHE_TIME)) {
+    return pageDataCache.getCachedValue();
+  }
+
+  const page = await n2m.pageToMarkdown(id);
+
+  await pageDataCache.save(page, "json");
+
+  return page;
 }
 
 async function getTILs() {
   const tils = [];
+  console.time("Time to get TIL Data");
   const rawTILs = await getTILData();
+  console.timeLog("Time to get TIL Data");
+  console.timeEnd("Time to get TIL Data");
 
   for (const rawTIL of rawTILs.results) {
-    const postMarkdownBlocks = await n2m.pageToMarkdown(rawTIL.id);
+    console.time("Time to get page data");
+    const postMarkdownBlocks = await pageToMarkdown(rawTIL.id);
+    console.timeLog("Time to get page data");
+    console.timeEnd("Time to get page data");
 
     tils.push({
       title: rawTIL.properties.title.title[0].plain_text,
@@ -37,7 +70,7 @@ async function getTILs() {
 }
 
 async function getHomePage() {
-  const homePageMarkdownBlocks = await n2m.pageToMarkdown(
+  const homePageMarkdownBlocks = await pageToMarkdown(
     process.env.NOTION_HOME_PAGE
   );
   return n2m.toMarkdownString(homePageMarkdownBlocks);
